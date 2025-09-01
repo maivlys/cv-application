58d7339ee4aff86ba0acca58d8235d1a6bbd821c
maivlys <122049862+maivlys@users.noreply.github.com>
2025-09-01 20:02:20 +0200

    typo

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -25,8 +25,8 @@ export default function Info({setInfoData}) {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <defs>
               <linearGradient id="gradEdit" x1="0%" y1="0%" x2="100%" y2="0%">
-                <stop offset="0%" stop-color="#29F39B" />
-                <stop offset="100%" stop-color="#02A1F9" />
+                <stop offset="0%" stopColor="#29F39B" />
+                <stop offset="100%" stopColor="#02A1F9" />
               </linearGradient>
             </defs>
       <path fill="url(#gradEdit)" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg>
@@ -36,21 +36,21 @@ export default function Info({setInfoData}) {
         <label>
           Name:
         </label>
-        <input type="text" autocomplete="off"
+        <input type="text" autoComplete="off"
         value={formData.name}
         onChange={(e)=>setFormData({...formData, name: e.target.value})}/>
 
         <label >
           Email:
         </label>
-        <input type="text" autocomplete="off"
+        <input type="text" autoComplete="off"
            value={formData.email}
            onChange={(e)=>setFormData({...formData, email: e.target.value})}/>
 
         <label >
           Phone:
         </label>
-        <input type="text" autocomplete="off"
+        <input type="text" autoComplete="off"
            value={formData.phone}
            onChange={(e)=>setFormData({...formData, phone: e.target.value})}/>
 
